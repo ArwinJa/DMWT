b847a0d82916e9e5f4e3d13ea3a25dd713aee872
ArwinJa <arwin.jacob.aj@gmail.com>
2025-01-17 15:46:48 +0100

    Raketenstarts Inhalt

--- components/textBox1.js
+++ components/textBox1.js
@@ -20,25 +20,20 @@ function TextBox1() {
                         <div className={styles.textBoxMain}>
                             <h2 className={styles.h2}>Raketenstarts</h2>
                             <p className={styles.p}>
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                                Es gibt eine Reihe von Anforderungen, die ein Weltraumbahnhof erfüllen muss, bevor er als 
+                                solcher in Frage kommt. Als erstes sollte er sich in der Nähe des Äquators befinden, denn die 
+                                höhere Geschwindigkeit verursacht durch die Erdrotation ermöglicht ein leichteres Aufsteigen in 
+                                der Atmosphäre. 
                             </p>
                             <p className={styles.p}>
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                                Außerdem sollte ein Weltraumbahnhof abseits von dicht besiedelten Gebieten liegen, da die Gefahr von 
+                                herunterfallenden Trümmern besteht und in etwas weiterer Entfernung auch abgestoßene Boosterraketen. 
                             </p>
                             <p className={styles.p}>
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                                Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                                Ein weiteres Risiko sind potenziell giftige Treibstoffreste. 
+                                Durch das Ausweichen auf dünn besiedelte Gebiete werden diese Aspekte eine Gefahr für
+                                unberührte Regionen.
+
                             </p>
                         </div>
                     </div>
